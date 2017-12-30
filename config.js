e2f77316fd2951ec07cd76fed209978e7c5d9497
piekario13 <piekario13@gmail.com>
2017-12-30 22:02:34 +0100

    zad_17_7

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID':'742864781403-7mn7n6tvonfoom243c7g3abdqkirugth.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET':'WsssBxVe1FJrDrcwuKC_hgWj',
+    'CALLBACK_URL':'http://localhost:3000/auth/google/callback'
+}
